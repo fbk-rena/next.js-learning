b660972ac5f767293b26831504f56ca3596a3508
Karla Lorena <lorena@safe2choose.org>
2025-08-27 21:18:43 -0600

    fix: rename posts lib file

---
+++ src/lib/posts.ts
@@ -0,0 +1,24 @@
+// src/lib/posts.ts
+export type Post = {
+  slug: string
+  title: string
+  date: string
+  content: string
+}
+
+export async function getPosts(): Promise<Post[]> {
+  return [
+    {
+      slug: 'post-1',
+      title: 'First post',
+      date: '2025-08-20',
+      content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    },
+    {
+      slug: 'post-2',
+      title: 'Second post',
+      date: '2025-08-25',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+  ]
+}
